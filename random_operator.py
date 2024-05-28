2139135df4b34af38a505e728afb15de124a3486
LegendaryEvenWeeb <62778359+Maho-Yoshino@users.noreply.github.com>
2024-05-28 20:03:53 +0200

    Big changes
    Including:
    - Debug mode
    - More advanced random picking
    - 5 operators at once instead of one

--- random_operator.py
+++ random_operator.py
@@ -1,67 +1,73 @@
-import random, json, os, logging, math
+import random, json, os, logging
 from datetime import datetime
 from typing import Literal
+# -============Logging=============-
 logger = logging.getLogger(__name__)
 filename = f"r6randomop_{str(datetime.now().date()).replace("-","_")}.log"
 log_format = "%(asctime)s::%(levelname)-8s:%(message)s"
 logging.basicConfig(filename=filename, encoding='utf-8', level=logging.DEBUG, format=log_format, datefmt="%H:%M:%S")
-
-json_name = "op_list.json"
-#json_name = "operator_list.json"
-
+# -=============Debug==============-
+debug:bool=True
+if debug:
+    json_name = "op_list.json"
+else:
+    json_name = "operator_list.json"
+# -==========Main Code=============-
 def main():
-    '''mode = input("Mode: ").lower()
-    if mode == "":
-        logging.error("Invalid mode input")
-        raise ValueError(f"Not a valid input (empty string)")
-    elif mode[0] == "a":
-        mode = "attack"
-        altmode = "defense"
-        logging.info("Mode: attack")
-    elif mode[0] == "d":
-        mode = "defense"
-        altmode = "attack"
-        logging.info("Mode: defense")
-    else:
-        logging.error("Invalid mode input")
-        raise ValueError(f"Not a valid input \"{mode}\"")
-    gmode = input("Gamemode (for single operator enter \"o\"): ").lower()[0]'''
-    # -=======-
-    gmode = "q"
-    mode = "attack"
-    altmode = "defense"
-    op_repeat = True
-    # -=======-
-    rounds = {
-        "rounds_per_side":0,
-        "OT":0
-    }
-    if gmode == "q":
-        rounds["rounds_per_side"] = 2
-        rounds["OT"] = 1
-        logging.info("Gamemode: Quick match")
-    elif gmode == "s":
-        rounds["rounds_per_side"] = 3
-        rounds["OT"] = 1
-        logging.info("Gamemode: Standard")
-    elif gmode == "r":
-        rounds["rounds_per_side"] = 3
-        rounds["OT"] = 3
-        logging.info("Gamemode: Ranked")
-    elif gmode == "o":
-        print(f"Random {mode} operator: {pick_random_op(mode, 1)}")
+    mode, altmode, op_repeat, rounds = data_processing()
+    if None in [mode, altmode, op_repeat, rounds]:
         return
-    else:
-        logging.error("Invalid gamemode")
-        raise ValueError("Invalid gamemode")
-    #op_repeat = input("Repeating operators?: ").lower()[0]
     mode_ops = []
     altmode_ops = []
     rounds_per_side = rounds["rounds_per_side"]+rounds["OT"]
     for i in range(rounds_per_side*5+1):
         mode_ops.append(pick_random_op(mode, rounds_per_side, mode_ops, True if op_repeat == "y" else False))
         altmode_ops.append(pick_random_op(altmode, rounds_per_side, altmode_ops, True if op_repeat == "y" else False))
-    print(mode_ops)
+    for j in range(1, rounds["rounds_per_side"]*2+1):
+        if j<=rounds["rounds_per_side"]:
+            print(f"round {j} ({mode}): {';'.join(mode_ops[j-1])}")
+        else:
+            print(f"round {j} ({altmode}): {';'.join(altmode_ops[j-rounds["rounds_per_side"]-1])}")
+    for i in range(1, rounds["OT"]+1):
+        round_num = rounds['rounds_per_side']*2+i
+        print(f"round {round_num} (overtime {mode}): {';'.join(mode_ops[rounds['rounds_per_side']-1+i])}")
+        print(f"round {round_num} (overtime {altmode}): {';'.join(altmode_ops[rounds['rounds_per_side']-1+i])}")
+
+def data_processing():
+    if debug:
+        return "attack","defense", False, {"rounds_per_side":2,"OT":1}
+    else:
+        mode = input("Mode: ").lower()
+        if mode == "":
+            logging.error("Invalid mode input")
+            raise ValueError(f"Not a valid input (empty string)")
+        mode = "attack" if mode[0].lower()=="a" else "defense"
+        altmode = "defense" if mode == "attack" else "attack"
+        gmode = input("Gamemode (for single operator enter \"o\"): ").lower()[0]
+        rounds = {
+            "rounds_per_side":0,
+            "OT":0
+        }
+        if gmode == "q":
+            rounds["rounds_per_side"] = 2
+            rounds["OT"] = 1
+            logging.info("Gamemode: Quick match")
+        elif gmode == "s":
+            rounds["rounds_per_side"] = 3
+            rounds["OT"] = 1
+            logging.info("Gamemode: Standard")
+        elif gmode == "r":
+            rounds["rounds_per_side"] = 3
+            rounds["OT"] = 3
+            logging.info("Gamemode: Ranked")
+        elif gmode == "o":
+            print(f"Random {mode} operator: {pick_random_op(mode, 1)}")
+            return None, None, None, None
+        else:
+            logging.error("Invalid gamemode")
+            raise ValueError("Invalid gamemode")
+        op_repeat = input("Repeating operators?: ").lower()[0]
+        return mode, altmode, op_repeat, rounds
 
 def open_file():
     try: 
@@ -80,16 +86,24 @@ def valid_operators(side:Literal["attack", "defense"]):
 def pick_random_op(side:Literal["attack", "defense"], rounds:int, exclusions:list = None, repeat:bool=False):
     if exclusions is None:
         exclusions = []
-    logging.debug(f"pick random op:\tside: {side}\trepeat:{repeat}\trounds: {rounds}\texclusions: {exclusions}")
+    operators = []
     v_op = valid_operators(side)
-    operator = v_op[random.randint(0, len(v_op)-1)]
-    loops = 0
-    while loops<10 and ((repeat and operator == exclusions[-1] and len(v_op)>=2) or (not repeat and operator not in exclusions and len(v_op)>=3)):
+    if len(v_op)<2:
+        raise ValueError("aint no way fam")
+    if len(v_op)<10 and not repeat:
+        print(f"Not enough operators to start ({side}), turning repeating to on.")
+        repeat = True
+    logging.debug(f"pick random op:\tside: {side}\trepeat:{repeat}\trounds: {rounds}\texclusions: {exclusions}")
+    for i in range(5):
         operator = v_op[random.randint(0, len(v_op)-1)]
-        logging.debug(f"{operator} was already taken, retrying...")
-        loops += 1
-    logging.debug(f"pick_random_operator({side}):{operator}")
-    return operator
+        while not ((repeat and len(exclusions)>0 and operator in exclusions[-1]) or (not repeat and len(exclusions)>0 and operator in exclusions[-1])) and (len(exclusions)>0 and operator in exclusions[-1]) and len(v_op)>1:
+            operator = v_op[random.randint(0, len(v_op)-1)]
+        else:
+            operators.append(operator)
+        if len(v_op)>5:
+            v_op.remove(operator)
+        logging.debug(f"pick_random_operator({side}):{operator}")
+    return operators
 op_list = open_file()
 def init():
     while True:
@@ -100,4 +114,5 @@ def init():
         tmp = input("")
         if tmp != "":
             os._exit(0)
-init()
+if __name__ == "__main__":
+    init()
